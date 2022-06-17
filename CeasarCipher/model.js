5609d579f5196599542030d97ddf1857d88ff083
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-06-17 17:10:51 +0200

    First commit.
    Added some variables and made it display to the screen.

---
+++ CeasarCipher/model.js
@@ -0,0 +1,10 @@
+// The text that the user inputs. Will be transformed using a cipher function.
+let plainText = [];
+
+// Stores the plain text after it has been encrypted.
+let encryptedText = [];
+
+
+// The key that the cipher uses to encrypt it (can also be used to decrypt it?). 
+// Should probably not store it her. Will fix later.
+let key = ""; 
---
+++ CeasarCipher/view.js
@@ -0,0 +1,8 @@
+let appView = document.getElementById('app');
+
+view();
+function view(){
+    let html = /*HTML*/`Something`;
+
+    appView.innerHTML = html;
+}
